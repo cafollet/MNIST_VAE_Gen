ae74d075de8f91da0dd6815ec28885471d7a2357
Callum Follett <callumf2011@gmail.com>
2023-05-23 22:53:03 -0700

    Change paramfile, added comments to data.py, created requirements.txt

--- data.py
+++ data.py
@@ -4,9 +4,8 @@ import numpy as np
 # Defining a Data Class
 class Data:
     def __init__(self):
-        x_train = np.delete(np.loadtxt("data/mnist_train.csv", skiprows=1, delimiter=','), 0, 1)
-        y_train = np.loadtxt("data/mnist_train.csv", skiprows=1, usecols=0, delimiter=',')  # useful for labels in
-        # future
+        x_train = np.delete(np.loadtxt("data/mnist_train.csv", skiprows=1, delimiter=','), 0, 1)  # Image training dataset
+        y_train = np.loadtxt("data/mnist_train.csv", skiprows=1, usecols=0, delimiter=',')  # Label training dataset
 
         label_train = y_train
 
